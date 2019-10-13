45384dddd8743e33ec871c38c5e87e6a651f2435
Serg <mcgorky@gmail.com>
2019-10-14 01:29:52 +0300

    feat: create webpack

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'script.js'
+  },
+  mode: 'development',
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [
+        MiniCssExtractPlugin.loader,
+        'css-loader',
+        'sass-loader'
+      ],
+    }, {
+      test: /\.html$/,
+      use: 'html-loader',
+    }, {
+      test: /\.(png|svg)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: './assets/img',
+          publicPath: './assets/img'
+        }
+      }
+    }],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.css'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
+};
